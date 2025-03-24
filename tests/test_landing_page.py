38645d3620cd7894b854062c27e18ec3bc9fdbc0
Anastasiya Kuznetsova <forquest@icloud.com>
2025-03-24 14:55:12 +0300

    написано 2 теста. разбираюсь с аллюром

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -1,5 +1,5 @@
 import allure
-from selene import browser, have
+from selene import have, be
 
 
 @allure.epic("Landing Page Tests")
@@ -12,4 +12,17 @@ def test_logo_is_visible(browser):
         browser.open("https://it.arda.digital")
     with allure.step("Проверяем, что в хедере есть изображение с логотипом"):
         browser.element("header img").should(have.attribute("src"))
+
+@allure.epic("Landing Page Tests")
+@allure.feature("Registration Form")
+@allure.story("Форма регистрации должна открываться по клику на кнопку")
+@allure.title("Проверка открытия формы регистрации")
+
+def test_registration_form_opens(browser):
+    with allure.step("Открываем страницу https://it.arda.digital"):
+        browser.open("https://it.arda.digital")
+    with allure.step("Кликаем на кнопку регистрации"):
+        browser.element("span.w-full.relative.inline-block.px-8.py-3.text-sm.font-bold.tracking-widest.text-black.uppercase.border-2.border-current.group-active\\:text-opacity-75").click()
+    with allure.step("Проверяем, что форма регистрации отображается"):
+        browser.element("form").should(be.visible)
 
